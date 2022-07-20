711d85fb2d577bcc8a8ff5625d4d50b9e66051ba
DmitriiBalakirev <dmitry.balgar@gmail.com>
2022-07-20 20:36:41 +0300

    1st task push

---
+++ Assets/Scripts/DogMoveSystem.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using Unity.Entities;
+public class DogMoveSystem : ComponentSystem
+{
+    private EntityQuery _query;
+    protected override void OnCreate()
+    {
+        _query = GetEntityQuery(ComponentType.ReadOnly<DogMoveComponent>());
+    }
+    protected override void OnUpdate()
+    {
+        Entities.With(_query).ForEach((Entity entity, Transform transform, DogMoveComponent dogMove) => 
+        {
+            var pos = transform.position;
+            pos.y += dogMove.MoveSpeed/1000;
+            transform.position = pos;
+        });
+    }
+}
+
